1341366d1880cd0279f430cc02fe4f9ed0d087a2
Oscar Rovira <oscar.rovira.casanova@gmail.com>
2011-09-04 03:48:18 +0200

    Show names in two columns by gender

--- src/main/java/com/beayoscar/babynames/web/NameController.java
+++ src/main/java/com/beayoscar/babynames/web/NameController.java
@@ -1,12 +1,38 @@
 package com.beayoscar.babynames.web;
 
-import com.beayoscar.babynames.domain.Name;
+import java.util.Collection;
+
 import org.springframework.roo.addon.web.mvc.controller.RooWebScaffold;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.beayoscar.babynames.domain.Name;
 
 @RooWebScaffold(path = "names", formBackingObject = Name.class)
 @RequestMapping("/names")
 @Controller
 public class NameController {
+
+	@ModelAttribute("names")
+    public Collection<Name> populateNames() {
+        //return Name.findAllNames();
+		return null;
+    }
+	
+//	@ModelAttribute("male_names")
+//	public Collection<Name> populateMaleNames() {
+//		return Name.findNamesByGender(Gender.MALE).getResultList();
+//	}
+//	@ModelAttribute("female_names")
+//	public Collection<Name> populateFemaleNames() {
+//		return Name.findNamesByGender(Gender.FEMALE).getResultList();
+//	}
+	
+    @RequestMapping(value = "/listgenders", method = RequestMethod.GET)
+    public String list(Model uiModel) {
+        return "names/listbygender";
+    }
 }
