ca9ff0a6ca404b2ec64ca9e421f0dde44d9829e1
Oscar Rovira <oscar.rovira.casanova@gmail.com>
2011-09-07 19:53:03 +0200

    AJAX vote support. Needs to improve the control of votes

--- src/main/java/com/beayoscar/babynames/web/NameController.java
+++ src/main/java/com/beayoscar/babynames/web/NameController.java
@@ -2,10 +2,14 @@ package com.beayoscar.babynames.web;
 
 import java.util.Collection;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.roo.addon.web.mvc.controller.RooWebScaffold;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -22,17 +26,20 @@ public class NameController {
 		return null;
     }
 	
-//	@ModelAttribute("male_names")
-//	public Collection<Name> populateMaleNames() {
-//		return Name.findNamesByGender(Gender.MALE).getResultList();
-//	}
-//	@ModelAttribute("female_names")
-//	public Collection<Name> populateFemaleNames() {
-//		return Name.findNamesByGender(Gender.FEMALE).getResultList();
-//	}
-	
     @RequestMapping(value = "/listgenders", method = RequestMethod.GET)
     public String list(Model uiModel) {
         return "names/listbygender";
+    }
+    
+    @RequestMapping(value = "/{id}/liked", method = RequestMethod.PUT, headers = "Accept=application/json")
+    public ResponseEntity<String> updateFromJsonw(@PathVariable("id") Long id) {
+    	Name name = Name.findName(id);
+    	name.setVote(name.getVote() + 1);
+        HttpHeaders headers= new HttpHeaders();
+        headers.add("Content-Type", "application/text");
+        if (name.merge() == null) {
+            return new ResponseEntity<String>(headers, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<String>(name.toJson(), headers, HttpStatus.OK);
     }
 }
