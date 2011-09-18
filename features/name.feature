699a309e6280bb3463d808bd45434e9776b9bd76
Oscar Rovira <oscar.rovira.casanova@gmail.com>
2011-09-18 14:53:21 +0200

    Added integration tests with "test integration" on Name. Testing with Cucumber for Java. First scenario passed

---
+++ features/name.feature
@@ -0,0 +1,7 @@
+Feature: Hello World Feature
+        In order to ensure that my installation works
+        As a Developer
+        I want to run a quick Cuke4Duke test
+
+        Scenario: Hello World Scenario
+                I have a name
---
+++ src/test/java/com/beayoscar/babynames/domain/NameIntegrationTest.java
@@ -0,0 +1,12 @@
+package com.beayoscar.babynames.domain;
+
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = Name.class)
+public class NameIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
